3c691593854b5f77bd4886ccb24ac33de3eadb72
sajjad-source <f006b78@plank.thayer.dartmouth.edu>
2023-11-10 23:26:45 -0500

    Fixed random gold distribution. The error was that in certain maps, such as small.txt, the gold distrbution wasnt correct, and as a result, after collecting all the gold, there was still some left over. This would cause the game to not be possible to finish. I fixed this by making sure to take into account the map design, and also guarentee random distribution

--- gold.c
+++ gold.c
@@ -7,9 +7,11 @@
 // View declaration.h for more details
 
 void distribute_gold(GameMap* game_map, int goldTotal, int goldMinNumPiles, int goldMaxNumPiles) {
-
     // Determine the number of gold piles
     int numPiles = goldMinNumPiles + rand() % (goldMaxNumPiles - goldMinNumPiles + 1);
+
+    // Make sure we don't create more piles than we have empty spaces
+    numPiles = (numPiles < game_map->emptySpaceCount) ? numPiles : game_map->emptySpaceCount;
 
     // Allocate memory for the gold piles
     game_map->gold_piles = malloc(numPiles * sizeof(GoldPile));
@@ -21,31 +23,28 @@ void distribute_gold(GameMap* game_map, int goldTotal, int goldMinNumPiles, int 
     // Distribute gold among the piles
     int remainingGold = goldTotal;
     for (int i = 0; i < numPiles; i++) {
-        // Ensure each pile gets at least 1 gold piece
-        if (numPiles - i != 1) {
-            int goldInPile = (remainingGold > (numPiles - i)) ? (1 + rand() % (remainingGold - (numPiles - i) + 1)) : 1;
-            game_map->gold_piles[i].gold_count = goldInPile;
-            remainingGold -= goldInPile;
-        } else {
-            game_map->gold_piles[i].gold_count = remainingGold;
-            remainingGold -= remainingGold;
-        }
+        game_map->gold_piles[i].gold_count = 1;  // Assign at least one gold to each pile
+        remainingGold--;
     }
 
-    // printf("This is gold remainging: %d\n", remainingGold);
+    // Distribute the remaining gold
+    for (int i = 0; i < remainingGold; i++) {
+        int pileIndex = rand() % numPiles;
+        game_map->gold_piles[pileIndex].gold_count++;
+    }
+
+    // Shuffle the empty spaces to randomize gold pile placement
+    for (int i = 0; i < game_map->emptySpaceCount; i++) {
+        int swapIndex = rand() % game_map->emptySpaceCount;
+        Empty temp = game_map->emptySpaces[i];
+        game_map->emptySpaces[i] = game_map->emptySpaces[swapIndex];
+        game_map->emptySpaces[swapIndex] = temp;
+    }
 
     // Place gold piles on the map
     for (int i = 0; i < numPiles; i++) {
-        if (i < game_map->emptySpaceCount) {
-
-            int randomIndex = rand() % game_map->emptySpaceCount;
-
-            game_map->gold_piles[i].position[0] = game_map->emptySpaces[randomIndex].x;
-            game_map->gold_piles[i].position[1] = game_map->emptySpaces[randomIndex].y;
-
-            game_map->emptySpaces[randomIndex] = game_map->emptySpaces[game_map->emptySpaceCount - 1];
-            game_map->emptySpaceCount--;
-        }
+        game_map->gold_piles[i].position[0] = game_map->emptySpaces[i].x;
+        game_map->gold_piles[i].position[1] = game_map->emptySpaces[i].y;
     }
 
     game_map->numGoldPiles = numPiles;
